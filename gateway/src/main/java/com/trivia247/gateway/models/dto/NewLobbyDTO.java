1b0b0ada1d3e0fd1c9b32c7426db85a18669d711
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2023-05-13 12:21:51 -0300

    Revert "BROKEN: add initial app, api gateway, verbs service folders"
    This reverts commit 655214f77acd7bb9aaf6f09ec3d9eef8fa9873fc.

--- gateway/src/main/java/com/trivia247/gateway/models/dto/NewLobbyDTO.java
+++
@@ -1,8 +0,0 @@
-package com.trivia247.gateway.models.dto;
-
-public record NewLobbyDTO(
-		String name,
-		String ownerName,
-		String ownerUID,
-		Integer sizeLimit
-) {}
--- verbs/src/main/java/com/trivia247/gateway/models/dto/NewRoomDTO.java
+++
@@ -1,6 +0,0 @@
-package com.trivia247.gateway.models.dto;
-
-public record NewRoomDTO(
-		String name,
-		Integer sizeLimit
-) {}
